e081c51ef99c80250b285c14e06bd32d6e758bdd
Stephen Power <simpilldev@gmail.com>
2023-06-19 05:58:30 +0300

    Allow swipe right to close drawer

--- js/swipe.js
+++ js/swipe.js
@@ -12,8 +12,22 @@ document.getElementById("page").addEventListener('touchend', function (event) {
     handleGesture();
 }, false);
 
+document.getElementById("drawer-overlay").addEventListener('touchstart', function (event) {
+    touchstartX = event.changedTouches[0].screenX;
+    touchstartY = event.changedTouches[0].screenY;
+}, false);
+
+document.getElementById("drawer-overlay").addEventListener('touchend', function (event) {
+    touchendX = event.changedTouches[0].screenX;
+    touchendY = event.changedTouches[0].screenY;
+    handleGesture();
+}, false);
+
 function handleGesture() {
     if (touchendX > (touchstartX + 50)) {
         document.getElementById("drawer-input").checked = true;
+    }
+    if ((touchendX + 50) < touchstartX && document.getElementById("drawer-input").checked == true) {
+        document.getElementById("drawer-input").checked = false;
     }
 }
